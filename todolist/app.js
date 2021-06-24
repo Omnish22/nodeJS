19581381a41c1ffd24f6416043752bc0c638a235
Omnish Kumar <omnish22@gmail.com>
2021-06-25 00:15:10 +0530

    style sheet add to todolist

--- todolist/app.js
+++ todolist/app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+// to tell express that load public folders files
+app.use(express.static("public"));
 
 var items = ["Eating","Coding","Sleeping"];
 
